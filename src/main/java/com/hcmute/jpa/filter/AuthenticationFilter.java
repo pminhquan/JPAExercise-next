4baaef11746f8c3c54057f4e189208269acc4544
Quan <qm33165@gmail.com>
2026-08-28 21:23:56 +0700

    feat: add product pagination

--- src/main/java/com/hcmute/jpa/filter/AuthenticationFilter.java
+++ src/main/java/com/hcmute/jpa/filter/AuthenticationFilter.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/categories", "/products", "/products/*"})
+@WebFilter(urlPatterns = {"/categories", "/products", "/products/*", "/product", "/product/*"})
 public class AuthenticationFilter implements Filter {
 
     @Override
